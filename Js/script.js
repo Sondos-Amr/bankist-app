87d6d015bce5fcbda459d5e2d6f6e35dd1ec4223
Sondos Amr <sondos.amr444@gmail.com>
2025-07-11 19:55:14 +0300

    feat: implement displayMovements to render account transaction history dynamically

--- Js/script.js
+++ Js/script.js
@@ -68,3 +68,20 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
 // Functions
+
+const displayMovments = function (acc) {
+  acc.forEach((mov, i) => {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+            ${i + 1} ${type}
+          </div>
+          <div class="movements__value">${mov} €</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovments(account1.movements);
